55c50952b605fe67890459d96af33086c4b17c64
tushar20mehta <tushar20mehta@gmail.com>
2026-04-07 10:52:47 +0200

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { Routes, Route } from 'react-router-dom'
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
+import Entdecken from './pages/Entdecken'
+import Hosten from './pages/Hosten'
+import MeineDinner from './pages/MeineDinner'
+
+function App() {
+  return (
+    <div className="app">
+      <Navbar />
+      <main className="main-content">
+        <Routes>
+          <Route path="/" element={<Entdecken />} />
+          <Route path="/create" element={<Hosten />} />
+          <Route path="/my-dinners" element={<MeineDinner />} />
+        </Routes>
+      </main>
+      <Footer />
+    </div>
+  )
+}
+
+export default App
