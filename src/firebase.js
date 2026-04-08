fa9cf912ed53fd4fd6ec44d355e6eefea8eb06c9
tushar20mehta <tushar20mehta@gmail.com>
2026-04-08 23:30:14 +0200

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,19 +1,22 @@
 import { initializeApp } from 'firebase/app'
 import { getAuth, GoogleAuthProvider } from 'firebase/auth'
 import { getFirestore } from 'firebase/firestore'
+import { getStorage } from 'firebase/storage'
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDNiByycQvorgGnZmj6C0oWFTCF6Dkqi9E",
-    authDomain: "nevereatalone-2.firebaseapp.com",
-    projectId: "nevereatalone-2",
-    storageBucket: "nevereatalone-2.firebasestorage.app",
-    messagingSenderId: "245339629017",
-    appId: "1:245339629017:web:1052dadbd461cfd91c65c8",
-    measurementId: "G-0NK2Z23EVZ"
+  apiKey: "AIzaSyDNiByycQvorgGnZmj6C0oWFTCF6Dkqi9E",
+  authDomain: "nevereatalone-2.firebaseapp.com",
+  projectId: "nevereatalone-2",
+  storageBucket: "nevereatalone-2.firebasestorage.app",
+  messagingSenderId: "245339629017",
+  appId: "1:245339629017:web:1052dadbd461cfd91c65c8",
+  measurementId: "G-0NK2Z23EVZ"
 }
 
 const app = initializeApp(firebaseConfig)
 export const auth = getAuth(app)
 export const googleProvider = new GoogleAuthProvider()
 export const db = getFirestore(app)
+export const storage = getStorage(app)
+
 export default app
